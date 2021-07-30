9a3194293ef2620d999ffd75165f5d3d8bcc55bd
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-07-30 16:52:33 +0100

    passed Room test test_room_has_name_My_Little_Pony

--- tests/room_test.py
+++ tests/room_test.py
@@ -9,3 +9,7 @@ class TestRoom(unittest.TestCase):
     def test_room_has_name_Hello_Kitty(self):
         room_1 = Room("Hello Kitty")
         self.assertEqual("Hello Kitty", room_1.room_name)
+
+    def test_room_has_name_My_Little_Pony(self):
+        room_1 = Room("My Little Pony")
+        self.assertEqual("My Little Pony", room_1.room_name)
