47fe0458689d191e5a78ef4b98b27f66c1de74ae
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-07-30 22:40:58 +0100

    passed caraoke bar test test_check_all_guests_out_of_all_rooms

--- src/caraoke_bar.py
+++ src/caraoke_bar.py
@@ -2,6 +2,7 @@ class CaraokeBar:
     def __init__(self,caraoke_bar_name):
 
         self.caraoke_bar_name = caraoke_bar_name
+        self.rooms =[]
         self.music_librairy = []
 
     def check_guest_into_room(self,Guest,Room):
@@ -10,6 +11,10 @@ class CaraokeBar:
     def check_guest_out_of_room(self,guest,Room):
         Room.room_occupants.remove(guest)
 
-    def check_all_guest_out_from_room(self,Room):
+    def check_all_guests_out_from_room(self,Room):
         Room.room_occupants.clear()
+
+    def check_all_guests_out_from_all_rooms(self):
+        for Room in self.rooms:
+            Room.room_occupants.clear()
 
