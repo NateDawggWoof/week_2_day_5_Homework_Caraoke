cb4598ab65d5024971bd4822f19d2c4563072e48
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-07-30 18:39:29 +0100

    passed Caraoke Bar test test_check_in_two_guests_into_room

--- tests/caraoke_bar_test.py
+++ tests/caraoke_bar_test.py
@@ -15,7 +15,12 @@ class TestCaraokeBar(unittest.TestCase):
     def test_caraoke_bar_has_name_Code_Clan_Caraoke(self):
         self.assertEqual("Code Clan Caraoke", self.caraoke_bar.caraoke_bar_name)
     
-    def test_check_in_guest_to_room(self):
+    def test_check_in_guest_into_room(self):
         self.caraoke_bar.check_guest_into_room(self.guest_1,self.room_1)
         self.assertEqual(1, self.room_1.check_number_of_occupants())
+
+    def test_check_in_two_guests_into_room(self):
+        self.caraoke_bar.check_guest_into_room(self.guest_1,self.room_1)
+        self.caraoke_bar.check_guest_into_room(self.guest_2,self.room_1)
+        self.assertEqual(2, self.room_1.check_number_of_occupants())
 
