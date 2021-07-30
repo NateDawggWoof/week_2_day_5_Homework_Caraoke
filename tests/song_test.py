b6cd36be486ca661cde75aa48d1e067a2a075922
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-07-30 17:34:04 +0100

    passed Room test test_song_has_band_artist_name_Survivor

--- tests/song_test.py
+++ tests/song_test.py
@@ -5,11 +5,16 @@ class TestSong(unittest.TestCase):
     
     # def setUp(self):
 
-    def test_song_has_name_Eye_Of_The_Tiger(self):
+    def test_song_has_song_name_Eye_Of_The_Tiger(self):
         song_1 = Song("Eye of the Tiger","Survivor")
         self.assertEqual("Eye of the Tiger", song_1.song_name)
 
 
-    def test_song_has_name_YMCA(self):
+    def test_song_has_song_name_YMCA(self):
         song_1 = Song("YMCA","Village people")
         self.assertEqual("YMCA",song_1.song_name)
+
+    
+    def test_song_has_band_or_artist_name_Survivor(self):
+        song_1 = Song("Eye of the Tiger","Survivor")
+        self.assertEqual("Survivor", song_1.band_or_artist_name)
