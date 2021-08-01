6cbfe403f82e2be32084660d5d593fbe7d1bfb08
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-01 21:48:31 +0100

    exstension complete

--- src/guest.py
+++ src/guest.py
@@ -1,9 +1,11 @@
 import random
 
 class Guest:
-    def __init__(self, name):
+    def __init__(self, name,caraoke_bar):
         self.guest_name = name
         self.debit_card = random.randint(0,100)
+        self.preffered_room_choice = random.choice(caraoke_bar.rooms)
+        self.only_wants_preffered_room = random.choice(yes, no)
 
 
 
